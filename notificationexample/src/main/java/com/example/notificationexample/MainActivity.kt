f96108fedb5b7ea0d8b1a263b8d962b5e8e44956
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2020-12-31 09:41:49 +0900

    Edit MainActivity

--- notificationexample/src/main/java/com/example/notificationexample/MainActivity.kt
+++ notificationexample/src/main/java/com/example/notificationexample/MainActivity.kt
@@ -17,23 +17,74 @@ import androidx.core.app.NotificationManagerCompat
 class MainActivity : AppCompatActivity() {
 
     var i = 0
+    lateinit var notificationManager : NotificationManager
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initNotificationManager()
+
 
         findViewById<Button>(R.id.button).setOnClickListener {
-            createNotification()
+            createBigTextNotification()
         }
 
     }
 
+    private fun initNotificationManager() {
+
+        notificationManager = this.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        setNotificationChannel()
+    }
+
+    private fun setNotificationChannel() {
+        //val notificationManager = this.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(
+                //id는 말 그대로 Notification의 ID값
+                NotificationChannel("Study_Android App", "Notification Test App", NotificationManager.IMPORTANCE_DEFAULT)
+            )
+        }
+    }
+
     private fun createNotification() {
+        val builder = NotificationCompat.Builder(this, "default")
+
+        val pendingIntent = PendingIntent.getActivity(baseContext, 0,
+            intent, PendingIntent.FLAG_UPDATE_CURRENT)    // 3
+
+        // Notification의 왼쪽 위에 뜨는 작은 아이콘
+        builder.setSmallIcon(android.R.mipmap.sym_def_app_icon)
+
+        // 아이콘 오른쪽, 맨 상단에 뜨는 Notification의 제목
+        builder.setContentTitle("알림 제목")
+        // Notification의 내용
+        builder.setContentText("Notification Test 입니다.")
+
+        //Small Icon의 색깔
+        builder.color = Color.BLUE
+        // 사용자가 탭을 클릭하면 자동 제거
+        builder.setAutoCancel(false)
+        builder.setContentIntent(pendingIntent)
+
+
+        // id값은 정의해야하는 각 알림의 고유한 int값
+        notificationManager.notify(i++, builder.build())
+    }
+
+
+    private fun createBigTextNotification() {
 
         val builder = NotificationCompat.Builder(this, "default")
 
         val style = NotificationCompat.BigTextStyle()
+
+        val pendingIntent = PendingIntent.getActivity(baseContext, 0,
+            intent, PendingIntent.FLAG_UPDATE_CURRENT)    // 3
+
 
         style.bigText("알람 세부 텍스트알람 세부 텍스트알람 세부 텍스트알람 세부 텍스트알람 세부 텍스트알람 세부 텍스트알람 세부 텍스트알람 세부 텍스트알람 세부 텍스트알람 세부 텍스트알람 세부 텍스트알람 세부 텍스트알람 세부 텍스트알람 세부 텍스트알람 세부 텍스트알람 세부 텍스트")
 
@@ -42,20 +93,9 @@ class MainActivity : AppCompatActivity() {
         builder.setContentTitle("알림 제목")
         builder.setContentText("테스트")
 
-        builder.color = Color.RED
-        // 사용자가 탭을 클릭하면 자동 제거
-        builder.setAutoCancel(true)
+        builder.setContentIntent(pendingIntent)   // 10
 
         builder.setStyle(style)
-
-
-        // 알림 표시
-        val notificationManager = this.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            notificationManager.createNotificationChannel(
-                NotificationChannel("default", "기본 채널", NotificationManager.IMPORTANCE_DEFAULT)
-            )
-        }
 
         // id값은
         // 정의해야하는 각 알림의 고유한 int값

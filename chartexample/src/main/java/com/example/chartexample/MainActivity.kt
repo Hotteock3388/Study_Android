d37bf8eb37de789814403a4fb7768c98cb08fcb1
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-05 23:26:44 +0900

    Update MainActivity.kt

--- chartexample/src/main/java/com/example/chartexample/MainActivity.kt
+++ chartexample/src/main/java/com/example/chartexample/MainActivity.kt
@@ -1,11 +1,77 @@
 package com.example.chartexample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.charts.RadarChart
+import com.github.mikephil.charting.data.RadarData
+import com.github.mikephil.charting.data.RadarDataSet
+import com.github.mikephil.charting.data.RadarEntry
+import com.github.mikephil.charting.interfaces.datasets.IRadarDataSet
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var radarChart: RadarChart
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        radarChart = findViewById(R.id.radarChart)
+
+        radarChart.setBackgroundColor(Color.WHITE)
+
+
+        radarChart.getDescription().setEnabled(false);
+
+        radarChart.setWebLineWidth(1f);
+        radarChart.setWebColor(Color.LTGRAY);
+        radarChart.setWebLineWidthInner(1f);
+        radarChart.setWebColorInner(Color.LTGRAY);
+        radarChart.setWebAlpha(100);
+
+
+        setData()
     }
+
+
+    private fun setData() {
+        val mul = 80f
+        val min = 20f
+        val cnt = 5
+        val entries1: ArrayList<RadarEntry> = ArrayList()
+        val entries2: ArrayList<RadarEntry> = ArrayList()
+
+        // NOTE: The order of the entries when being added to the entries array determines their position around the center of
+        // the chart.
+        for (i in 0 until cnt) {
+            val val1 = (Math.random() * mul).toFloat() + min
+            entries1.add(RadarEntry(val1))
+            val val2 = (Math.random() * mul).toFloat() + min
+            entries2.add(RadarEntry(val2))
+        }
+        val set1 = RadarDataSet(entries1, "Last Week")
+        set1.color = Color.rgb(103, 110, 129)
+        set1.fillColor = Color.rgb(103, 110, 129)
+        set1.setDrawFilled(true)
+        set1.fillAlpha = 180
+        set1.lineWidth = 2f
+        set1.isDrawHighlightCircleEnabled = true
+        set1.setDrawHighlightIndicators(false)
+
+        val sets: ArrayList<IRadarDataSet> = ArrayList()
+        sets.add(set1)
+        val data = RadarData(sets)
+        //data.setValueTypeface(tfLight)
+        data.setValueTextSize(8f)
+        data.setDrawValues(false)
+        data.setValueTextColor(Color.WHITE)
+        radarChart.setData(data)
+        radarChart.invalidate()
+    }
+
+
 }

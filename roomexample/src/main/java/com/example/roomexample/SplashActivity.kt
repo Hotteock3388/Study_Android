16c8219d94d81e0ac6dc774687f7745d3f492457
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-25 15:39:02 +0900

    Update setDummyData() 메서드 생성

---
+++ roomexample/src/main/java/com/example/roomexample/SplashActivity.kt
@@ -0,0 +1,45 @@
+package com.example.roomexample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    private var studentDB: StudentDB? = null
+    private var studentList = listOf<Student>()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        studentDB = StudentDB.getInstance(applicationContext)
+
+        setDummyData()
+
+
+    }
+
+    private fun setDummyData() {
+
+        val r = Runnable {
+            var cnt = 1
+            for(i in 0..1000){
+
+                studentDB?.studentDAO()?.insert(Student(cnt++, "$i 번째 김효선",19, "남"))
+                studentDB?.studentDAO()?.insert(Student(cnt++, "$i 번째 김상현",19, "남"))
+                studentDB?.studentDAO()?.insert(Student(cnt++, "$i 번째 유영재",19, "남"))
+                studentDB?.studentDAO()?.insert(Student(cnt++, "$i 번째 신이삭",19, "남"))
+                studentDB?.studentDAO()?.insert(Student(cnt++, "$i 번째 문상규",19, "남"))
+                studentDB?.studentDAO()?.insert(Student(cnt++, "$i 번째 김준우",18, "남"))
+
+            }
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+        }
+
+        val thread = Thread(r)
+        thread.start()
+
+    }
+}

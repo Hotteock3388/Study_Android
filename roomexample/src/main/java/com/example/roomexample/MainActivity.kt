54baa9b591324f924c91ec965445f7eac853729d
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-25 15:38:34 +0900

    Update getAll(), search(), setText()메서드 구현

--- roomexample/src/main/java/com/example/roomexample/MainActivity.kt
+++ roomexample/src/main/java/com/example/roomexample/MainActivity.kt
@@ -2,11 +2,68 @@ package com.example.roomexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var studentDB: StudentDB? = null
+    private var studentList = listOf<Student>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        studentDB = StudentDB.getInstance(this)
+
+        getAll()
+
+        button.setOnClickListener {
+            search()
+            //setText()
+        }
+    }
+
+    private fun getAll(){
+        val r = Runnable {
+            studentList = studentDB?.studentDAO()?.getAll()!!
+
+            //setText()
+
+        }
+
+        val thread = Thread(r)
+        thread.start()
+    }
+
+    private fun search(){
+        var handler = Handler()
+        val r = Runnable {
+            studentList = studentDB?.studentDAO()?.search( "%${editText.text.toString()}%")!!
+            setText(handler)
+            //findViewById<TextView>(R.id.textView).text = str
+        }
+
+        val thread = Thread(r)
+        thread.start()
+
+    }
+
+    private fun setText(handler: Handler){
+
+        val r = Runnable {
+            var str = ""
+            for(i in 0 until studentList.size){
+                str += " ${studentList[i].name} ${studentList[i].age} ${studentList[i].gender} \n"
+            }
+            findViewById<TextView>(R.id.textView).text = str
+        }
+
+
+        handler.post(r)
     }
 }

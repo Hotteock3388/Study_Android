6fad738c686228763c9391f8f1ed3e627057449a
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-29 23:57:12 +0900

    Student 클래스 -> 데이터 클래스

--- roomexample/src/main/java/com/example/roomexample/Student.kt
+++ roomexample/src/main/java/com/example/roomexample/Student.kt
@@ -5,10 +5,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "student")
-class Student(@PrimaryKey var classNum: Int,
-              @ColumnInfo(name = "name") var name: String,
-              @ColumnInfo(name = "age") var age: Int,
-              @ColumnInfo(name = "gender") var gender: String)
-{
-    constructor(): this("0000".toInt(), "호떡", 19, "남")
-}
+data class Student(@PrimaryKey var classNum: Int,
+                   var name: String,
+                   var age: Int,
+                   var gender: String)

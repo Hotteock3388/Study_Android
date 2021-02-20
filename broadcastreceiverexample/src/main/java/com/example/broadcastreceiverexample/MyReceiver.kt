220811a4423eaeecd8446155842170de24a4da14
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-20 22:38:40 +0900

    .

--- broadcastreceiverexample/src/main/java/com/example/broadcastreceiverexample/MyReceiver.kt
+++ broadcastreceiverexample/src/main/java/com/example/broadcastreceiverexample/MyReceiver.kt
@@ -11,13 +11,13 @@ class MyReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         if (Intent.ACTION_POWER_CONNECTED.equals(intent?.action)) {
-            Toast.makeText(context, "전원 연결", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "전원 연결", Toast.LENGTH_SHORT).show()
         }
         else if(myAction.equals(intent?.action)) {
-            Toast.makeText(context, "방송", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "방송", Toast.LENGTH_SHORT).show()
         }
         else{
-            Toast.makeText(context, "ACTION = ${intent?.action}", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "ACTION = ${intent?.action}", Toast.LENGTH_SHORT).show()
         }
 
     }

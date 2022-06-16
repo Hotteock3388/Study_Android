7d0ebc86217327d0f965a9ad8fbdf5c8cbdfd416
Hotteock <57486593+Hotteock3388@users.noreply.github.com>
2022-06-16 16:11:16 +0900

    .

--- listviewtest/src/main/java/com/example/listviewtest/MainActivity.kt
+++ listviewtest/src/main/java/com/example/listviewtest/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.listviewtest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import java.io.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,34 +11,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var cnt = 5
-        var temp = 0
+        val file = "${baseContext?.applicationInfo?.dataDir}/files/data.txt"
         var arrayList = ArrayList<String>()
-        for(i in 0..cnt){
-            arrayList.add("$i")
+
+        try {
+            val br = BufferedReader(InputStreamReader(FileInputStream(file), "x-windows-949"))
+
+
+            BufferedReader(InputStreamReader(FileInputStream(file), "x-windows-949")).use {
+                var line: String?
+                while (it.readLine().also { line = it } != null){
+                    Log.d("TestLog", "readLine = ${line.toString()}")
+                    if (line?.contains("존재")!!){
+                        arrayList.add(line!!)
+                    }
+                }
+            }
+
+        }catch (e: IOException){
+            e.printStackTrace()
         }
-//        Log.d("TestLog","For문 끝")
-//        Log.d("TestLog","arrayList = $arrayList")
-//        Log.d("TestLog","arrayListSize = ${arrayList.size}")
 
-        Singleton.setList(arrayList)
+        Log.d("TestLog", "size = ${arrayList.size}")
 
-        var myAdapter =  CustomAdapter(applicationContext, arrayList)
-        recyclerView.adapter = myAdapter
-        myAdapter.notifyDataSetChanged()
-
-
-        //Log.d("TestLog","Adapter 등록 끝")
-
-
-        button.setOnClickListener {
-            Log.d("TestLog", "Click!!!")
-            //arrayList.add("${temp++}")
-            Singleton.arrayList.add("22")
-            myAdapter.notifyDataSetChanged()
-        }
 
     }
-
 
 }
